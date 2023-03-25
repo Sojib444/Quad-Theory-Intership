b42880ef93d50e011acb73b6e23fa9ce8cd72ccb
sojib <mdsojibhosen444@gmail.com>
2023-03-25 21:06:57 +0600

    Working with Project

---
+++ src/Quad Theory Limited/Intership/Migrations/20230325150526_first.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intership.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "classTables",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModificationDate = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_classTables", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "studentTables",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Modificationdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClassIdID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_studentTables", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_studentTables_classTables_ClassIdID",
+                        column: x => x.ClassIdID,
+                        principalTable: "classTables",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_studentTables_ClassIdID",
+                table: "studentTables",
+                column: "ClassIdID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "studentTables");
+
+            migrationBuilder.DropTable(
+                name: "classTables");
+        }
+    }
+}
--- src/Quad Theory Limited/Intership/Program.cs
+++ src/Quad Theory Limited/Intership/Program.cs
@@ -1,7 +1,32 @@
+using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Internship.Infrastructure.Context;
+using Internship.Infrastructure.Muodule;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//connectionString
+var connectionString = builder.Configuration.GetConnectionString("ConnectionString");
+
+//AsseblyName
+var assemblyName = Assembly.GetExecutingAssembly().FullName;
+
+//Autofac Configuration
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+	containerBuilder.RegisterModule(new InfrastructureModule(connectionString, assemblyName));
+});
+
+//Set DBContext
+builder.Services.AddDbContext<ApplicationDbContext>(option => 
+option.UseSqlServer(connectionString, e => e.MigrationsAssembly(assemblyName)));
 
 var app = builder.Build();
 
@@ -12,6 +37,8 @@ if (!app.Environment.IsDevelopment())
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
